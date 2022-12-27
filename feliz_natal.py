4b70f10a21c495d3f986913d8ab5cb236f3350cb
Emerson <emersonmonteiro.costa@gmail.com>
2022-12-27 12:58:36 -0300

    refatoracao do codigo python e atualizacao do output(png)

--- feliz_natal.py
+++ feliz_natal.py
@@ -4,23 +4,24 @@ n = 0
 
 isaias_9_6 = '''
 
-	Porque um menino nos nasceu, um filho se nos deu.
-	O governo está sobre os seus ombros, e o seu nome será:
-	"Maravilhoso Conselheiro", "Deus Forte", 
-	"Pai da Eternidade", "Príncipe da Paz".
+    Porque um menino nos nasceu, um filho se nos deu.
+    O governo está sobre os seus ombros, e o seu nome será:
+    "Maravilhoso Conselheiro", "Deus Forte", 
+    "Pai da Eternidade", "Príncipe da Paz".
 
-	 Isaías 9:6
-	 
-             '''
-			 
-print(f'\n {isaias_9_6}')
+    Isaías 9:6
+
+    '''
+
+print(f'\n{isaias_9_6}')
 
 while n < 10:
 	if n == 0:
-		print('\n                 Feliz Natal!\n\n                      *')
+		print('\n', (21*' '), f'Feliz Natal!\n\n', (26*' '), '*')
 	else:
-		print('            ',((b * (10-n)) + ((n + (n-1))) * a))
-		
+		print(18*' ', ((b * (10-n)) + ((n + (n-1))) * a))
+
 	n +=1
-	
-print('                      |\n                      |\n\n')
+
+print((27*' '),f'|\n',(26*' '),'|\n\n')
+
